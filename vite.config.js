6dee7c3710380b2de9904f655453d452d410b6d5
Ashmit Raj <trivediashmit3@gmail.com>
2024-10-03 00:24:38 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/react_resume/', // This should match your repo name
 })
